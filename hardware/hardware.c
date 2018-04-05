15a8fc1409eae2f32ad38467208b96536ec9853d
Mauro <mchiozzi95@gmail.com>
2018-04-05 16:31:59 -0300

    Adiocionando prototipos das funções

--- hardware/hardware.c
+++ hardware/hardware.c
@@ -1,7 +1,43 @@
- void setup(){
+#include <avr/io.h>
+#include <util/delay.h>
 
- }
+#define QNT_SENSORES 5
 
- void loop(){
- 	
- }
+struct SensorDistancia{
+
+};
+
+struct Coordenada{
+	float ponto_x;
+	float ponto_y;
+};
+
+float calcularAnguloRotacao(struct Coordenada inicio, struct Coordenada fim){
+	return 0;
+}
+
+float calcularDistanciaMovimento(struct Coordenada inicio, struct Coordenada fim){
+	return 0;
+}
+
+float *lerDistanciaSensores(){
+	float distancias_lidas[QNT_SENSORES] = {0};
+
+	return distancias_lidas;
+}
+
+void deslocamento(float angulo_rotacao, float distancia_translacao){
+
+}
+
+int main (void)
+{
+	struct Coordenada atual, seguinte;
+	
+	// Lógica GA
+
+	float angulo_rotacao = calcularAnguloRotacao(atual, seguinte);
+	float distancia_translacao = calcularDistanciaMovimento(atual, seguinte);
+
+	deslocamento(angulo_rotacao, distancia_translacao);
+}
